ffbe596251bdbe43c9fcbb4f0bed81191e740524
adichatupes-source <adichat.upes@gmail.com>
2026-02-23 13:40:43 +0530

    Update ServicesSection layout for responsive grid

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -52,7 +52,7 @@ export function ServicesSection() {
         </motion.div>
 
         {/* Services Grid */}
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {services.map((service, index) => (
             <motion.div
               key={service.title}
